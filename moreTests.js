7d3ffe2d4d7b51024cdc46b7c9a98e3670ae61f1
Alf Magne Kalleland <alf@papirfly.no>
2018-05-31 00:12:43 +0200

    more tests

---
+++ moreTests.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// jshint expr: true
+
+var chai = require("chai"),
+    expect = chai.expect;
+
+
+chai.should();
+
+describe("more tests", function(){
+    it("should be true", function(){
+        expect(true).to.be.true;
+    })
+
+});
--- test.js
+++ test.js
@@ -8,44 +8,50 @@ var chai = require("chai"),
 
 chai.should();
 
-function isEven(num){
+function isEven(num) {
     return num % 2 === 0;
 }
 
-describe("is even", function(){
 
-    it("should return true when number is even", function(){
-        isEven(4).should.be.true;
-    });
-
-    it("should return false when numer is odd", function(){
-        expect(isEven(5)).to.be.false;
-    });
-});
-
-function add(num1, num2){
+function add(num1, num2) {
     return num1 + num2;
 }
 
-describe("with setUp and tearDown", function(){
+describe("number tests", function () {
+    describe("is even", function () {
 
-    var num;
+        it("should return true when number is even", function () {
+            isEven(4).should.be.true;
+        });
 
-    beforeEach(function(){
-        num = 5;
+        it("should return false when numer is odd", function () {
+            expect(isEven(5)).to.be.false;
+        });
     });
 
-    afterEach(function(){
 
+
+    describe("with setUp and tearDown", function () {
+
+        var num;
+
+        beforeEach(function () {
+            num = 5;
+        });
+
+        afterEach(function () {
+
+        });
+
+        it("should be 10 when adding 5 to 5", function () {
+            num = add(num, 5);
+            num.should.equal(10);
+        });
+
+        xit("shoudl be 12 when adding 7 to 5", function () {
+            num = add(num, 7);
+            num.should.equal(12);
+        });
     });
 
-    it("should be 10 when adding 5 to 5", function(){
-        num = add(num, 5);
-        num.should.equal(10);
-    });
-
-    it("shoudl be 12 when adding 7 to 5", function(){
-        num = add(num, 7);
-        num.should.equal(12);
-    });
 });
